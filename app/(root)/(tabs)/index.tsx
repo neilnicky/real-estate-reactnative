f14f178cebf369fa10b8745cfd482a9c08bd11b4
neilnicky <neilnicholaskanat@gmail.com>
2025-03-07 12:29:55 +0530

    fix: typescript error

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-xl my-10">Welcome to Realestate!!</Text>
+      <Text className="font-bold text-3xl my-10 font-rubik">Welcome to Realestate!!</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
