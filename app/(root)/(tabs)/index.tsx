66df138a6b116dfb78e137b4434b4081a2933d00
neilnicky <neilnicholaskanat@gmail.com>
2025-03-07 12:08:03 +0530

    feat: add assets and constants

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-xl my-10">Welcome to Realestate!!</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
